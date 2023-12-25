4c413c7a81e21fac60602816764b565a28fd12a0
Patryk Mielech <patryk.mielech@protonmail.com>
2023-12-25 11:19:42 +0100

    VideoCaptureThread
    Created thread to contain all the video capture related functions.

--- interface.py
+++ interface.py
@@ -1,10 +1,68 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from PyQt5.QtCore import Qt
-from main import update_image, init_camera, video_loop, vid, cam_process
+from PyQt5.QtCore import Qt, QTimer, QDateTime, QThread, QEventLoop
+from main import cv2
+
+relative_app = None
+frame = None
+vid_object = None
+
+
+def init_camera():
+    global width, height, vid_object
+    vid_object = cv2.VideoCapture(0)
+    vid_object.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    vid_object.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+
+def numpy_to_pixmap(numpy_image):
+    num_height, num_width, channel = numpy_image.shape
+    q_image = QImage(numpy_image.data, num_width, num_height, QImage.Format_RGBA8888)
+
+    return QPixmap.fromImage(q_image)
+
+
+def get_frame(video: cv2.VideoCapture):
+    _, frame = video.read()
+    frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_NEAREST)
+
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+
+
+def video_loop(self, video: cv2.VideoCapture, flag):
+    global frame
+    frame = get_frame(video)
+    if frame is not None:
+        pixmap = numpy_to_pixmap(frame)
+        self.image_label.setPixmap(pixmap)
+
+
+def collectProcessData():
+    global relative_app
+    print("Collecting Process Data")
+    print(type(relative_app))
+
+
+class VideoCaptureThread(QThread):
+
+    def __init__(self, *args, **kwargs):
+        QThread.__init__(self, *args, **kwargs)
+        self.dataCollectionTimer = QTimer()
+        self.dataCollectionTimer.moveToThread(self)
+        self.dataCollectionTimer.timeout.connect(video_loop())
+
+    def run(self):
+        self.dataCollectionTimer.start(1000)
+        loop = QEventLoop()
+        loop.exec_()
+
+    def setRelativeApp(self, pass_app):
+        global relative_app
+        relative_app = pass_app
 
 
 class Window(QWidget):
+    width, height = 800, 600
 
     def __init__(self):
         super().__init__()
@@ -47,6 +105,18 @@ class Window(QWidget):
         self.setLayout(layout)
         self.set_empty_image()
 
+    def cam_process(self, video: cv2.VideoCapture):
+        video = init_camera()
+        self.timer = self.QTimer(self)  # Create Timer
+        if video is not None:
+            self.timer.timeout.connect(video_loop(self, video))  # Connect timeout to the output function
+            self.timer.start(40)  # emit the timeout() signal at x=40ms
+            # video_loop(app, video, True)
+
+    def updateUi_image(self, image):
+        pixmap = numpy_to_pixmap(image)
+        self.image_label.setPixmap(pixmap)
+
     def set_empty_image(self):
         empty_image = QImage(800, 600, QImage.Format_RGB32)
         empty_image.fill(Qt.white)
@@ -67,30 +137,33 @@ class Window(QWidget):
     #         pixmap = QPixmap(file_path)
     #         self.image_label.setPixmap(pixmap)
 
-    def button_click_(self):
-        update_image(self)
+    # def button_click_(self):
+    #     update_image(self)
 
     def button_click_Load(self):
         pass
 
     def button_click_Start(self):
-        global vid
-        index = self.comboBox.currentIndex()
-
-        if index == 0:
-            cam_process(self, vid)
-
-        elif index == 1:
-
-            pass
-
-        elif index == 2:
-
-            pass
+        self.videoCollectionThread = VideoCaptureThread()
+        self.videoCollectionThread.setTerminationEnabled(True)
+        self.videoCollectionThread.setRelativeApp(self)
+        self.videoCollectionThread.start()
+        # global vid
+        # index = self.comboBox.currentIndex()
+        #
+        # if index == 0:
+        #     cam_process(self, vid)
+        #
+        # elif index == 1:
+        #
+        #     pass
+        #
+        # elif index == 2:
+        #
+        #     pass
 
     def handle_combobox_change(self, index):
         if index == 0:
             self.load_button.setEnabled(False)
         else:
             self.load_button.setEnabled(True)
-
--- main.py
+++ main.py
@@ -13,60 +13,10 @@ userInput = sys.argv
 app = None
 
 
-def init_camera():
-    global width, height
-    video = cv2.VideoCapture(0)
-    video.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    video.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    return video
-
-
-def get_frame(video: cv2.VideoCapture):
-    _, frame = video.read()
-    frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_NEAREST)
-
-    return cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-
-
-def numpy_to_pixmap(numpy_image):
-    num_height, num_width, channel = numpy_image.shape
-    q_image = QImage(numpy_image.data, num_width, num_height, QImage.Format_RGBA8888)
-
-    return QPixmap.fromImage(q_image)
-
-
-def video_loop(app: QWidget, video: cv2.VideoCapture, flag):
-        frame = get_frame(video)
-        if frame is not None:
-            pixmap = numpy_to_pixmap(frame)
-            app.image_label.setPixmap(pixmap)
-
-
-
-def cam_process(app: QWidget, video: cv2.VideoCapture):
-    video = init_camera()
-    app.timer = app.QTimer(app)  # Create Timer
-    if video is not None:
-        app.timer.timeout.connect(video_loop(app, video))  # Connect timeout to the output function
-        app.timer.start(40)  # emit the timeout() signal at x=40ms
-        #video_loop(app, video, True)
-
-
-def update_image(self: QWidget):
-    global vid, img
-    if vid is None:
-        vid = init_camera()
-    else:
-        frame = get_frame(vid)
-        if frame is not None:
-            pixmap = numpy_to_pixmap(frame)
-            self.image_label.setPixmap(pixmap)
-
-
 if __name__ == '__main__':
     # vid = init_camera()
-
     app = QApplication(sys.argv)
+
     window = Window()
     window.show()
     sys.exit(app.exec_())
